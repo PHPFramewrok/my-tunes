59b33f978713b6ee5a916cbd95c692e2c8d2f01c
Andrew Baumgart <andrew.s.baumgart@gmail.com>
2015-03-24 20:44:15 -0500

    'php artisan optimize' in order to execute migration rollbacks

--- vendor/compiled.php
+++ vendor/compiled.php
@@ -17099,7 +17099,6 @@ class AppServiceProvider extends ServiceProvider
     }
     public function register()
     {
-        $this->app->bind('Illuminate\\Contracts\\Auth\\Registrar', 'App\\Services\\Registrar');
     }
 }
 namespace App\Providers;
